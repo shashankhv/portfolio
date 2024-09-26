d513a73fd4189be210590b9c522d7cfb334a5f23
shashankhv <hvshashank@hotmail.com>
2024-09-26 13:39:12 -0500

    updated env variables

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -60,14 +60,14 @@ const Projects = () => {
               ))}
             </div>
 
-            <a
+            {/* <a
               className="flex items-center gap-2 cursor-pointer text-white-600"
               href={currentProject.href}
               target="_blank"
               rel="noreferrer">
               <p>Check Live Site</p>
               <img src="/assets/arrow-up.png" alt="arrow" className="w-3 h-3" />
-            </a>
+            </a> */}
           </div>
 
           <div className="flex justify-between items-center mt-7">
